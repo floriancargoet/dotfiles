f604d5800b94cb6e1e2ff8a667415f13493afda6
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2011-12-22 10:54:04 +0100

    added .config/geany

--- setup.sh
+++ setup.sh
@@ -3,15 +3,33 @@
 #paths
 SCRIPT_FILE=$(readlink -f $0)
 SCRIPT_DIR=$(dirname $SCRIPT_FILE)
+ORIG_DIR=$(pwd)
+
+install_file(){
+    if [ ! -e "$HOME/.$1" ]; then
+        ln -s "$(readlink -f $1)" "$HOME/.$1" && echo "$1 installed"
+    else
+        echo "~/.$1 already exists"
+    fi
+}
 
 cd "$SCRIPT_DIR/dotfiles"
-for file in *
-do
-    if [ ! -e "$HOME/.$file" ]
-    then 
-        ln -s "$(readlink -f $file)" "$HOME/.$file" && echo "$file installed"
-    else
-        echo "~/.$file already exists"
+
+# automated installation
+
+for file in * ; do
+    if [ -f $file ]; then
+        install_file $file
     fi
 done
-cd - > /dev/null
+
+# manual installation
+#geany
+if [ ! -e "$HOME/.config/geany" ]; then
+    ln -s "$(readlink -f config/geany)" "$HOME/.config/geany" && echo "config/geany/ installed"
+else
+    echo "~/.config/geany/ already exists"
+fi
+
+
+cd $ORIG_DIR
